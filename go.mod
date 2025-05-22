01e0bf7abb07009045bf8ab892f1748b5d7a0218
yangbo <yangbo@yungengxin.com>
2025-05-22 17:12:28 +0800

    效率优化

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21
 
 require (
 	github.com/dreadl0ck/tlsx v1.0.2
+	github.com/panjf2000/gnet/v2 v2.7.2
 	github.com/redis/go-redis/v9 v9.3.0
 	github.com/refraction-networking/utls v1.6.7
 )
@@ -15,6 +16,12 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/google/gopacket v1.1.17 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/panjf2000/ants/v2 v2.11.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
--- proxy/handler.go
+++ proxy/handler.go
@@ -10,120 +10,108 @@ import (
 	"tls-proxy/config"
 	"tls-proxy/fingerprint"
 	"tls-proxy/util"
+
+	"github.com/panjf2000/gnet/v2"
 )
 
-// StartProxy 监听 listenAddr，转发到 forwardAddr，并进行 JA3/JA3S 指纹检查（根据开关控制）
-func StartProxy(listenAddr, forwardAddr string) error {
-	ln, err := net.Listen("tcp", listenAddr)
-	if err != nil {
-		return err
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			slog.Warn("接收连接失败", "err", err)
-			continue
-		}
-		go handle(conn, forwardAddr)
-	}
+type proxyServer struct {
+	gnet.BuiltinEventEngine
+	forwardAddr string
 }
 
-func handle(clientConn net.Conn, forwardAddr string) {
-	defer clientConn.Close()
+type connContext struct {
+	handshakeDone bool
+	clientBuffer  []byte
+	targetConn    net.Conn
+}
 
-	// 读取客户端初始数据（预读 8KB 数据，用于解析 ClientHello）
-	clientConn.SetReadDeadline(time.Now().Add(2 * time.Second))
-	clientBuf := make([]byte, 8192)
-	n, err := clientConn.Read(clientBuf)
-	if err != nil {
-		slog.Error("读取客户端握手数据失败", "err", err, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-		return
+func (ps *proxyServer) OnOpened(c gnet.Conn) (out []byte, action gnet.Action) {
+	c.SetContext(&connContext{})
+	return
+}
+
+func (ps *proxyServer) OnClosed(c gnet.Conn, err error) (action gnet.Action) {
+	ctx := c.Context().(*connContext)
+	if ctx.targetConn != nil {
+		ctx.targetConn.Close()
 	}
-	clientData := clientBuf[:n]
+	return
+}
 
-	// 如果开启 JA3+ 检查，则提取并判断 JA3+ 指纹
-	EnableJA3Check := config.EnableJA3Check()
-	EnableJA3Collection := config.EnableJA3Collection()
-	EnableJA3NCheck := config.EnableJA3NCheck()
-	EnableJA3NCollection := config.EnableJA3NCollection()
-	if EnableJA3Check || EnableJA3Collection || EnableJA3NCheck || EnableJA3NCollection {
+func (ps *proxyServer) OnTraffic(c gnet.Conn) (action gnet.Action) {
+	ctx := c.Context().(*connContext)
+	data, _ := c.Next(-1)
+
+	if !ctx.handshakeDone {
+		ctx.clientBuffer = append(ctx.clientBuffer, data...)
+		if len(ctx.clientBuffer) < 5 {
+			return
+		}
+		clientData := ctx.clientBuffer
+		clientIP := strings.Split(c.RemoteAddr().String(), ":")[0]
+
 		if util.IsTLSClientHello(clientData) {
-			ja3Str, ja3nStr, err := fingerprint.JA3Fingerprint(&clientData)
-			if err != nil {
-				slog.Warn("提取 JA3+ 失败", "err", err, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-			} else {
-				slog.Debug("client ja3+ fingerprint", "ja3", ja3Str, "ja3n", ja3nStr, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-				if EnableJA3Collection {
-					config.ReportJA3(ja3Str)
-				}
-				if EnableJA3Check && config.ShouldBlockJA3(ja3Str) {
-					config.ReportJA3BlockedEvent(ja3Str)
-					slog.Info("[BLOCK] 客户端 JA3 阻断匹配，断开连接", "ja3", ja3Str, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-					return
-				}
-				if EnableJA3NCollection {
-					config.ReportJA3N(ja3nStr)
-				}
-				if EnableJA3NCheck && config.ShouldBlockJA3N(ja3nStr) {
-					config.ReportJA3NBlockedEvent(ja3nStr)
-					slog.Info("[BLOCK] 客户端 JA3N 阻断匹配，断开连接", "ja3n", ja3nStr, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-					return
+			if config.EnableJA3Check() || config.EnableJA3Collection() || config.EnableJA3NCheck() || config.EnableJA3NCollection() {
+				ja3Str, ja3nStr, err := fingerprint.JA3Fingerprint(&clientData)
+				if err == nil {
+					if config.EnableJA3Collection() {
+						config.ReportJA3(ja3Str)
+					}
+					if config.EnableJA3Check() && config.ShouldBlockJA3(ja3Str) {
+						config.ReportJA3BlockedEvent(ja3Str)
+						slog.Info("[BLOCK] JA3", "ja3", ja3Str, "ip", clientIP)
+						return gnet.Close
+					}
+					if config.EnableJA3NCollection() {
+						config.ReportJA3N(ja3nStr)
+					}
+					if config.EnableJA3NCheck() && config.ShouldBlockJA3N(ja3nStr) {
+						config.ReportJA3NBlockedEvent(ja3nStr)
+						slog.Info("[BLOCK] JA3N", "ja3n", ja3nStr, "ip", clientIP)
+						return gnet.Close
+					}
 				}
 			}
-		} else {
-			slog.Debug("客户端数据非 TLS ClientHello，不执行 JA3 检查", "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-		}
-	} else {
-		slog.Debug("客户端 JA3+ 检查已关闭，跳过", "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-	}
 
-	// 如果开启 JA4 检查，则提取并判断 JA4 指纹
-	EnableJA4Check := config.EnableJA4Check()
-	EnableJA4Collection := config.EnableJA4Collection()
-	if EnableJA4Check || EnableJA4Collection {
-		if util.IsTLSClientHello(clientData) {
-			ja4Str, err := fingerprint.JA4Fingerprint(&clientData)
-			if err != nil {
-				slog.Warn("提取 JA4 失败", "err", err, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-			} else {
-				slog.Debug("client ja4 fingerprint", "ja4", ja4Str, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-				if EnableJA4Collection {
-					config.ReportJA4(ja4Str)
-				}
-				if EnableJA4Check && config.ShouldBlockJA4(ja4Str) {
-					config.ReportJA4BlockedEvent(ja4Str)
-					slog.Info("[BLOCK] 客户端 JA4 阻断匹配，断开连接", "ja4", ja4Str, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-					return
+			if config.EnableJA4Check() || config.EnableJA4Collection() {
+				ja4Str, err := fingerprint.JA4Fingerprint(&clientData)
+				if err == nil {
+					if config.EnableJA4Collection() {
+						config.ReportJA4(ja4Str)
+					}
+					if config.EnableJA4Check() && config.ShouldBlockJA4(ja4Str) {
+						config.ReportJA4BlockedEvent(ja4Str)
+						slog.Info("[BLOCK] JA4", "ja4", ja4Str, "ip", clientIP)
+						return gnet.Close
+					}
 				}
 			}
-		} else {
-			slog.Debug("客户端数据非 TLS ClientHello，不执行 JA4 检查", "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
 		}
-	} else {
-		slog.Debug("客户端 JA3+ 检查已关闭，跳过", "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-	}
 
-	// 与目标服务器建立连接
-	targetConn, err := net.Dial("tcp", forwardAddr)
-	if err != nil {
-		slog.Error("连接目标服务器失败", "err", err, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
+		targetConn, err := net.DialTimeout("tcp", ps.forwardAddr, 2*time.Second)
+		if err != nil {
+			slog.Error("连接目标失败", "err", err)
+			return gnet.Close
+		}
+		ctx.targetConn = targetConn
+		ctx.handshakeDone = true
+
+		go func() {
+			defer c.Close()
+			targetConn.Write(clientData)
+			io.Copy(c, targetConn)
+		}()
+
 		return
 	}
-	defer targetConn.Close()
 
-	// 将客户端已读取的数据立即发送给目标服务器
-	_, err = targetConn.Write(clientData)
-	if err != nil {
-		slog.Error("向目标服务器转发数据失败", "err", err, "localaddr", strings.Split(clientConn.LocalAddr().String(), ":")[0])
-		return
+	if ctx.targetConn != nil {
+		ctx.targetConn.Write(data)
 	}
+	return
+}
 
-	// 清除所有超时设置
-	targetConn.SetReadDeadline(time.Time{})
-	clientConn.SetReadDeadline(time.Time{})
-
-	// 启动双向数据转发
-	go io.Copy(targetConn, clientConn)
-	// 使用 targetReader 以确保 Peek 后的数据不会丢失（若已使用）
-	io.Copy(clientConn, targetConn)
+func StartProxy(listenAddr, forwardAddr string) error {
+	ps := &proxyServer{forwardAddr: forwardAddr}
+	return gnet.Run(ps, "tcp://"+listenAddr, gnet.WithMulticore(true), gnet.WithReusePort(true))
 }
